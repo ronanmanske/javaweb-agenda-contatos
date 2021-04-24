af34dfcbf2a1ac3df765481268fc1665d61ade2c
Ronan Alberto Manske <69127@PROWAY.TREINA>
2021-04-24 17:45:41 -0300

    redirecionando para a pagina de novo contato

--- src/main/java/br/com/agendacontato/controller/ContatoController.java
+++ src/main/java/br/com/agendacontato/controller/ContatoController.java
@@ -1,5 +1,31 @@
 package br.com.agendacontato.controller;
 
-public class ContatoController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet(urlPatterns = {"/ContatoController","/main"})
+public class ContatoController extends HttpServlet {
+
+	private static final long serialVerionUID = 1L;
+	
+	
+	public ContatoController() {
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String action = request.getServletPath();
+		if(action.equals("/main")) {
+			listaContatos(request, response);
+		}
+	}
+	
+	protected void listaContatos(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+		response.sendRedirect("agenda.jps");
+	}
 }
