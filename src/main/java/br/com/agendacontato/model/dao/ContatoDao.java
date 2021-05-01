84ab3b0e483277f87aa59a461894f15f131dadc5
Ronan Alberto Manske <69127@PROWAY.TREINA>
2021-05-01 16:21:38 -0300

    daoContato metodo findAll

--- src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
+++ src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
@@ -1,47 +1,59 @@
 package br.com.agendacontato.model.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.agendacontato.infra.ConexaoJdbc;
 import br.com.agendacontato.infra.ConexaoMysql;
 import br.com.agendacontato.model.entity.Contato;
 
-
-
 public class ContatoDao implements BaseDao<Contato, Long> {
 
 	private final ConexaoJdbc conexao;
-    private String statement;
+	private String statement;
 
-    public ContatoDao() throws SQLException, ClassNotFoundException {
-        this.conexao = new ConexaoMysql();
-    }
-	
+	public ContatoDao() throws SQLException, ClassNotFoundException {
+		this.conexao = new ConexaoMysql();
+	}
+
 	@Override
-	public void create(Contato entity) throws SQLException {
+	public void create(Contato contato) throws SQLException {
 		statement = "INSERT INTO contato(nome, fone, email, observacao) values(?, ?, ?, ?)";
 
-        try (PreparedStatement preparedStatement = this.conexao.getConnection().prepareStatement(statement)) {
-            preparedStatement.setString(1, contato.getNome());
-            preparedStatement.setString(2, contato.getFone());
-            preparedStatement.setString(3, contato.getEmail());
-            preparedStatement.setString(4, contato.getObservacao());
+		try (PreparedStatement preparedStatement = this.conexao.getConnection().prepareStatement(statement)) {
+			preparedStatement.setString(1, contato.getNome());
+			preparedStatement.setString(2, contato.getFone());
+			preparedStatement.setString(3, contato.getEmail());
+			preparedStatement.setString(4, contato.getObservacao());
 
-            preparedStatement.execute();
-            this.conexao.commit();
-        } catch (SQLException e) {
-            this.conexao.rollback();
-            throw e;
-        }
-		
+			preparedStatement.execute();
+			this.conexao.commit();
+		} catch (SQLException e) {
+			this.conexao.rollback();
+			throw e;
+		}
+
 	}
 
 	@Override
 	public List<Contato> findAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		statement = "SELECT nome, fone, email, observacao FROM  contato";
+
+		try (PreparedStatement preparedStatement = this.conexao.getConnection().prepareStatement(statement);
+				ResultSet rs = preparedStatement.executeQuery();) {
+			List<Contato> contatoList = new ArrayList<>();
+			while (rs.next()) {
+				Contato c = new Contato(rs.getString("nome"), rs.getString("fone"), rs.getString("email"));
+				c.setObservacao(rs.getString("observacao"));
+				contatoList.add(c);
+			}
+			return contatoList;
+		} catch (SQLException e) {
+			throw e;
+		}
 	}
 
 	@Override
@@ -53,14 +65,13 @@ public class ContatoDao implements BaseDao<Contato, Long> {
 	@Override
 	public void update(Contato entity) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 }
--- src/main/java/br/com/agendacontato/model/entity/Contato.java
+++ src/main/java/br/com/agendacontato/model/entity/Contato.java
@@ -2,18 +2,16 @@ package br.com.agendacontato.model.entity;
 
 public class Contato {
 	
-	private int id ;
+	private int id;
     private String nome;
     private String fone;
     private String email;
     private String observacao;
 
-    public Contato(int id, String nome, String fone, String email, String observacao) {
-        this.id = id;
+    public Contato(String nome, String fone, String email) {
         this.nome = nome;
         this.fone = fone;
         this.email = email;
-        this.observacao = observacao;
     }
 
     public Contato() {
@@ -23,33 +21,19 @@ public class Contato {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getFone() {
         return fone;
     }
 
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getObservacao() {
         return observacao;
@@ -58,5 +42,6 @@ public class Contato {
     public void setObservacao(String observacao) {
         this.observacao = observacao;
     }
-
 }
+	
+
