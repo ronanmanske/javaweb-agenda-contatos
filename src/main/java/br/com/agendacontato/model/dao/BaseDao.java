e0df72a0e320348ddc0e052e4ea70a72a4b26882
Ronan Alberto Manske <69127@PROWAY.TREINA>
2021-05-01 16:36:03 -0300

    daoContato metodo findById

--- src/main/java/br/com/agendacontato/model/dao/BaseDao.java
+++ src/main/java/br/com/agendacontato/model/dao/BaseDao.java
@@ -1,5 +1,23 @@
 package br.com.agendacontato.model.dao;
 
-public interface BaseDao {
+import java.sql.SQLException;
+import java.util.List;
+
+
+
+/**
+ * Definir as assinaturas dos metodos de CRUD
+ */
+public interface BaseDao<T, ID> {
+
+    void create(T entity) throws SQLException;
+
+    List<T> findAll() throws SQLException;
+
+    T findById(ID id) throws SQLException;
+
+    void update(T entity);
+
+    void delete(ID id);
 
 }
--- src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
+++ src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
@@ -57,9 +57,22 @@ public class ContatoDao implements BaseDao<Contato, Long> {
 	}
 
 	@Override
-	public Contato findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Contato findById(Long id) throws SQLException {
+		Contato c = null;
+		statement = "SELECT nome, fone, email, observacao FROM contato WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = this.conexao.getConnection().prepareStatement(statement);
+             ResultSet rs = preparedStatement.executeQuery();
+        ) {
+            preparedStatement.setLong(1, id);
+            while (rs.next()) {
+                c = new Contato(rs.getString("nome"), rs.getString("fone"), rs.getString("email"));
+                c.setObservacao(rs.getString("observacao"));
+            }
+            return c;
+        } catch (SQLException e) {
+            throw e;
+        }
 	}
 
 	@Override
