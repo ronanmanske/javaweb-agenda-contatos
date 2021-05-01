fef183916c38407898fcd4e510f90f706042eb5a
Ronan Alberto Manske <69127@PROWAY.TREINA>
2021-05-01 16:16:01 -0300

    daoContato metodo create

--- src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
+++ src/main/java/br/com/agendacontato/model/dao/ContatoDao.java
@@ -1,5 +1,66 @@
 package br.com.agendacontato.model.dao;
 
-public class ContatoDao {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.agendacontato.infra.ConexaoJdbc;
+import br.com.agendacontato.infra.ConexaoMysql;
+import br.com.agendacontato.model.entity.Contato;
+
+
+
+public class ContatoDao implements BaseDao<Contato, Long> {
+
+	private final ConexaoJdbc conexao;
+    private String statement;
+
+    public ContatoDao() throws SQLException, ClassNotFoundException {
+        this.conexao = new ConexaoMysql();
+    }
+	
+	@Override
+	public void create(Contato entity) throws SQLException {
+		statement = "INSERT INTO contato(nome, fone, email, observacao) values(?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = this.conexao.getConnection().prepareStatement(statement)) {
+            preparedStatement.setString(1, contato.getNome());
+            preparedStatement.setString(2, contato.getFone());
+            preparedStatement.setString(3, contato.getEmail());
+            preparedStatement.setString(4, contato.getObservacao());
+
+            preparedStatement.execute();
+            this.conexao.commit();
+        } catch (SQLException e) {
+            this.conexao.rollback();
+            throw e;
+        }
+		
+	}
+
+	@Override
+	public List<Contato> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contato findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Contato entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
